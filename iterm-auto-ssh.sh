b20f3c44ea995f507b8668bd447e5f263ea0a8dd
Ben Browning <bbrowning@llnw.com>
2013-12-01 11:30:01 -0700

    Added debugging switch and renamed init() to prevent namespace collision when the script is sourced from something else (eg .bashrc)

--- iterm-auto-ssh.sh
+++ iterm-auto-ssh.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Written by thelinuxgirl
+# https://github.com/thelinuxgirl/iterm-auto-ssh/
 # Auto-ssh - auto ssh to defined hosts based on iterm session ID's #
 # Mac Iterm2 users
 # Set the Hostname values to the ITERM_SESSION_ID for
@@ -10,6 +12,8 @@
 # specified list of tabs and reconnects to a specific
 # screen for that window/tab/pane
 
+# Debugging var, for debug output
+#$_debugging=1
 
 ##################### DEPENDENCIES: #######################################
 # - Must use/src bssh function snippet from bashrc.snippet.               #
@@ -19,7 +23,7 @@
 ###########################################################################
 
 
-init(){
+autossh_init(){
 ## config file with host-id key pairs ##
 config=~/iterm-auto-ssh/auto-ssh.conf
 
@@ -34,7 +38,7 @@ fi
 if [[ -f $config ]]; then
 	. ${config}
 else
-	echo "can't find auto-ssh.conf at $config"
+	echo "can't find config file at $config"
 	return 1
 fi
 }
@@ -48,30 +52,27 @@ ssh_connect() {
  wtid=${id%%p[0-9]}
  wid=${id%%t[0-9]p[0-9]}
 
- echo "id: $id"
- echo "wtpid: $wtpid "
- echo "wtid: $wtid "
- echo "wid: $wid "
+ [[ $_debugging ]] && echo "id: $id  wtpid: $wtpid  wtid: $wtid  wid: $wid "
 
 
  ### Session ID check ###
  ### Iterate over session array, and check session id's ###
 
- echo "session arr ${session_arr[*]} "
+ [[ $_debugging ]] && echo "session arr ${session_arr[*]} "
 
  for session in ${session_arr[*]}; 
  do
 	## Pull hostnames for sessions in array ##
-	echo "session array: $session"
+	[[ $_debugging ]] && echo "session array: $session"
 
 	## indirectly reference session variable, to pull out session id list ##
 	session_ids=( ${!session} )
-	echo "session array ids: ${session_ids[*]} "
+	[[ $_debugging ]] && echo "session array ids: ${session_ids[*]} "
 
 	## iterate over session id's for defined host/session, and check against current session ID ##
 	for session_id in ${session_ids[*]};
 	do
-	echo "session id: $session_id"
+	[[ $_debugging ]] && echo "session id: $session_id"
 		if echo $wtpid | grep $session_id || {
 			echo $wtid | grep $session_id
 			} || { 
@@ -85,7 +86,7 @@ ssh_connect() {
 		fi
 	done
 	if ${host_set} ; then
-		echo "Set session to ${ssh_host} with id ${id} - spawning ssh connect"
+		[[ $_debugging ]] && echo "Set session to ${ssh_host} with id ${id} - spawning ssh connect"
 		break
 	else
 		echo "host $session doesn't match this iterm session ID"
@@ -94,16 +95,13 @@ done
 
 ## No match found - abort ##
 if [[ -z ${ssh_host} ]]; then
-	echo "error: no host assigned to these sessions: $wtpid , $wtid , $wid , aborting auto-ssh" 
+	[[ $_debugging ]] && echo "error: no host assigned to these sessions: $wtpid , $wtid , $wid , aborting auto-ssh" 
 	return 1
 else
- 	echo "final ssh host set: $ssh_host"
+ 	[[ $_debugging ]] && echo "final ssh host set: $ssh_host"
 fi
 
- echo "id: $id"
- echo "wtpid: $wtpid "
- echo "wtid: $wtid "
- echo "wid: $wid "
+ [[ $_debugging ]] && echo "id: $id wtpid: $wtpid wtid: $wtid wid: $wid "
 
 ### ssh connect stanza ###
 	if [[ -n $ssh_host ]] ; then
@@ -113,5 +111,5 @@ fi
 	fi
 }
 
-init
+autossh_init
 ssh_connect
